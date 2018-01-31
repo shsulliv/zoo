3697e657b30a7b4fe5e833ca9b886db63f25001d
Shannon Sullivan <shsulliv@gmail.com>
2018-01-31 14:57:07 +0000

    Added edit and update methods, modified index method of KeeperController

--- app/controllers/KeeperController.java
+++ app/controllers/KeeperController.java
@@ -2,6 +2,7 @@ package controllers;
 
 import io.ebean.Ebean;
 import models.Keeper;
+import models.Pen;
 import play.data.DynamicForm;
 import play.data.FormFactory;
 import play.mvc.Controller;
@@ -9,12 +10,13 @@ import play.mvc.Result;
 
 import javax.inject.Inject;
 import java.util.List;
+import java.util.UUID;
 
 public final class KeeperController extends Controller {
   @Inject private FormFactory formFactory;
 
   public Result index() {
-    List<Keeper> keepers = Ebean.find(Keeper.class).findList();
+    List<Keeper> keepers = Ebean.find(Keeper.class).fetch("pens").orderBy("keeperName").findList();
     return ok(views.html.keepers.index.render(keepers));
   }
 
@@ -29,11 +31,17 @@ public final class KeeperController extends Controller {
     return redirect("/keeper");
   }
 
-  public Result edit() {
-    return ok(views.html.keepers.edit.render());
+  public Result edit(UUID id) {
+    Keeper keeper = Ebean.find(Keeper.class, id);
+    List<Pen> pens = Ebean.find(Pen.class).findList();
+    return ok(views.html.keepers.edit.render(keeper, pens));
   }
 
-  public Result update() {
+  public Result update(UUID id) {
+    DynamicForm form = formFactory.form().bindFromRequest();
+    Pen pen = Ebean.find(Pen.class, UUID.fromString(form.get("keeper_pen")));
+    pen.keeper = Ebean.find(Keeper.class, id);
+    pen.save();
     return redirect("/keeper");
   }
 }
