d1ce1b886ffe973ab18d6383053803ea6595c39e
Shannon Sullivan <shsulliv@gmail.com>
2018-02-04 17:10:36 +0100

    Fixed weather data in HomeController

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -9,12 +9,13 @@ import play.mvc.Result;
 public class HomeController extends Controller {
   private static final String API_KEY = "5561a8e5d927fc2891afef569aaad7f9";
   private static final OWM weatherClient = new OWM(API_KEY);
+  private static final double kelvin = 273.15;
   /**
    * An action that renders an HTML page with a welcome message. The configuration in the <code>
    * routes</code> file means that this method will be called when the application receives a <code>
    * GET</code> request with a path of <code>/</code>.
    */
   public Result index() throws APIException {
-    return ok(views.html.index.render(weatherClient.currentWeatherByCityName("Barcelona")));
+    return ok(views.html.index.render(weatherClient.currentWeatherByCityName("London"), kelvin));
   }
 }
